a4889b81803de94e7e42258570cfa210a42a0abd
Santiago Rodriguez <scollazo@gmail.com>
2014-11-22 04:52:41 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,13 @@
+# Docker container for the ripple audience response system
+
+ ___Ripple is a free Audience/Personal Response System to add enhanced interactivity to classroom environment.___ ( [source]( https://interactivemedia.uoregon.edu/tools/ripple ) )
+ 
+ * Source: https://github.com/uoregon-libraries/ripple (sadly, it's no longer maintained)
+ 
+ * The first time the container is used, it will populate the db.
+ 
+ * Default credentials:
+ 
+		 user: Administrator
+       		 password: ripple
+         
---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if ! [ -f /var/lib/mongodb/ripple.0 ]
+	then
+	echo "First run, populating database"
+	chown mongodb.mongodb /var/lib/mongodb -R 
+	#Start mongo
+	/etc/init.d/mongodb start
+	echo -n "Waiting for mongodb socket ..."
+	while ! [ -a /var/run/mongodb/mongodb-27017.sock ]
+	        do
+		 echo -n "."
+       		 sleep 5
+        	done
+	echo  " ready!"
+
+	#Create ripple database and default Administrator user (password: ripple)
+	cd /opt/ripple/ 
+	make db-migrate 
+	# Stop mongod
+	mongod -f /etc/mongodb.conf --shutdown && rm -f /opt/firstrun.sh
+	fi
+
+supervisord
+
+
